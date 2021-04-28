32acb6bc8381ba617bb75027ae5d40d9f4ee1974
rluria14 <ryanlur14@gmail.com>
2021-04-28 14:20:44 -0400

    more edits to build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -114,6 +114,12 @@ mkdir -p tmp_build && pushd tmp_build
   make install-strip -j${CPU_COUNT}
   make texlinks
 
+    # At this point BLFS does:
+  # tar -xf ../../texlive-20180414-texmf.tar.xz -C /opt/texlive/2018 --strip-components=1
+  # .. but we would like to avoid this 2.5GB of stuff.
+  [[ -d "${SHARE_DIR}/texmf-dist" ]] || mkdir -p "${SHARE_DIR}/texmf-dist"
+  cp -rf "${SRC_DIR}"/texmf/texmf-dist/* "${SHARE_DIR}/texmf-dist/"
+
 #  if [[ ! ${target_platform} =~ .*linux.* ]]; then
 #    VERBOSE=1 LC_ALL=C make check ${VERBOSE_AT}
 #  elif [[ ${TEST_SEGFAULT} == yes ]] && [[ ${target_platform} =~ .*linux.* ]]; then
